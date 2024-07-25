eaa36d1be400ffee5299cf223318a402d4888701
pranayadna <pranayadnaap96@gmail.com>
2024-07-25 22:18:53 +0700

    Add components for charts

---
+++ src/components/ProductionChart.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { ref } from 'vue'
+
+const series = ref([44, 55, 13, 43, 22])
+
+const chartOptions = ref({
+  chart: {
+    type: 'pie',
+    height: 350
+  },
+  labels: ['Product A', 'Product B', 'Product C', 'Product D', 'Product E'],
+  responsive: [
+    {
+      breakpoint: 480,
+      options: {
+        chart: {
+          width: 200
+        },
+        legend: {
+          position: 'bottom'
+        }
+      }
+    }
+  ]
+})
+</script>
+
+<template>
+  <div>
+    <apexchart type="pie" :options="chartOptions" :series="series"></apexchart>
+  </div>
+</template>
---
+++ src/components/ReplacementChart.vue
@@ -0,0 +1,63 @@
+<script setup>
+import { ref } from 'vue'
+
+const series = ref([
+  {
+    name: 'Product A',
+    data: [44, 55, 41, 67, 22, 43, 21, 49, 62]
+  },
+  {
+    name: 'Product B',
+    data: [13, 23, 20, 8, 13, 27, 33, 12, 10]
+  },
+  {
+    name: 'Product C',
+    data: [11, 17, 15, 15, 21, 14, 13, 23, 20]
+  }
+])
+
+const chartOptions = ref({
+  chart: {
+    type: 'bar',
+    height: 350,
+    stacked: true,
+    zoom: {
+      enabled: true
+    }
+  },
+  responsive: [
+    {
+      breakpoint: 480,
+      options: {
+        legend: {
+          position: 'bottom',
+          offsetX: -10,
+          offsetY: 0
+        }
+      }
+    }
+  ],
+  plotOptions: {
+    bar: {
+      horizontal: false
+    }
+  },
+  xaxis: {
+    type: 'category',
+    categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep']
+  },
+  legend: {
+    position: 'right',
+    offsetY: 40
+  },
+  fill: {
+    opacity: 1
+  }
+})
+</script>
+
+<template>
+  <div>
+    <apexchart type="bar" :options="chartOptions" :series="series"></apexchart>
+  </div>
+</template>
